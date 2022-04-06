8fd9aca16a2e6def14b8140255ee0cc2af50ee5d
Rahul <Rk1821389@gmail.com>
2022-04-06 11:50:51 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by ios on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
